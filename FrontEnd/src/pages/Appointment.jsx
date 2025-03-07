f6da56e507a8d0673cea5e4dd04bc9ba741c793d
pnQuanq <phamquang13103@gmail.com>
2025-03-07 23:50:43 +0700

    add related-doctors in doc-page, Create about page

--- FrontEnd/src/pages/Appointment.jsx
+++ FrontEnd/src/pages/Appointment.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 import { assets } from '../assets/assets'
+import RelatedDoctors from '../components/RelatedDoctors'
 
 const Appointment = () => {
   const {docId} = useParams()
@@ -153,6 +154,7 @@ const Appointment = () => {
           </button>
         )}
       </div>
+      <RelatedDoctors docId={docId} speciality={docInfo.speciality}/>
     </div>
   )
 }
